24a86c79aa36125ecb42f06abdf57e6ace50ded3
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-10-26 15:01:07 +0900

    18.0 Header

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,20 @@
 import { Outlet } from "react-router-dom";
+import { Box, Button, HStack } from "@chakra-ui/react";
+import { FaAirbnb } from "react-icons/fa";
 
 export default function Root() {
     return (
-        <h1>
-            im root
+        <Box>
+            <HStack px={10} py={5} borderBottomWidth={1} justifyContent={"space-between"}>
+                <Box color={"red.500"}>
+                    <FaAirbnb size={48} />
+                </Box>
+                <HStack spacing={2}>
+                    <Button>Log in</Button>
+                    <Button colorScheme={"red"}>Sign up</Button>
+                </HStack>
+            </HStack>
             <Outlet />
-        </h1>
+        </Box>
     );
 }
