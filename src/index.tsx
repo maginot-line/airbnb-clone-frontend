071ead2c4309149ecddf5094f527aa1f87c3c19b
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-10-29 18:31:58 +0900

    19.2 React Query

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import theme from "./theme";
 
+const client = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <ChakraProvider theme={theme}>
-            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-            <RouterProvider router={router} />
-        </ChakraProvider>
+        <QueryClientProvider client={client}>
+            <ChakraProvider theme={theme}>
+                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+                <RouterProvider router={router} />
+            </ChakraProvider>
+        </QueryClientProvider>
     </React.StrictMode>
 );
