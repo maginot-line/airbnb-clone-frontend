7d3cc59f51ed424d18aac45b6beaa5e9aa02dda3
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-10-29 18:29:22 +0900

    19.0 Manual Fetching

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,36 @@
-import { Box, Grid } from "@chakra-ui/react";
+import { Grid } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Room from "../components/Room";
 import RoomSkeleton from "../components/RoomSkeleton";
 
+interface IPhoto {
+    pk: string;
+    file: string;
+    description: string;
+}
+
+interface IRoom {
+    pk: number;
+    name: string;
+    country: string;
+    city: string;
+    price: number;
+    rating: number;
+    is_owner: boolean;
+    photos: IPhoto[];
+}
 export default function Home() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [rooms, setRooms] = useState<IRoom[]>([]);
+    const fetchRooms = async () => {
+        const response = await fetch("http://127.0.0.1:8000/api/v1/rooms/");
+        const json = await response.json();
+        setRooms(json);
+        setIsLoading(false);
+    };
+    useEffect(() => {
+        fetchRooms();
+    }, []);
     return (
         <Grid
             mt={10}
@@ -10,9 +39,24 @@ export default function Home() {
             rowGap={8}
             templateColumns={{ sm: "1fr", md: "repeat(2, 1fr)", lg: "repeat(3,1fr)", xl: "repeat(4,1fr)", "2xl": "repeat(5,1fr)" }}
         >
-            <Box>
-                <RoomSkeleton />
-            </Box>
+            {isLoading ? (
+                <>
+                    {[...Array(10)].map((_, i) => (
+                        <RoomSkeleton />
+                    ))}
+                </>
+            ) : null}
+            {rooms.map((room) => (
+                <Room
+                    key={room.pk}
+                    imageUrl={room.photos[0].file}
+                    name={room.name}
+                    rating={room.rating}
+                    city={room.city}
+                    country={room.country}
+                    price={room.price}
+                />
+            ))}
         </Grid>
     );
 }
