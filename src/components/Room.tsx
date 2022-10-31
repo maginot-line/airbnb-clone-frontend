ce001926c70332a590d033c785a7e459ad0b20bc
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-10-31 17:33:36 +0900

    19.4 Room Detail

--- src/components/Room.tsx
+++ src/components/Room.tsx
@@ -1,7 +1,9 @@
 import { Box, Button, Grid, HStack, Image, Text, useColorModeValue, VStack } from "@chakra-ui/react";
 import { FaRegHeart, FaStar } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 interface IRoomProps {
+    pk: number;
     imageUrl: string;
     name: string;
     rating: number;
@@ -10,33 +12,35 @@ interface IRoomProps {
     price: number;
 }
 
-export default function Room({ imageUrl, name, rating, city, country, price }: IRoomProps) {
+export default function Room({ pk, imageUrl, name, rating, city, country, price }: IRoomProps) {
     const gray = useColorModeValue("gray.600", "gray.300");
     return (
-        <VStack alignItems={"flex-start"}>
-            <Box position={"relative"} overflow={"hidden"} mb={2} rounded={"2xl"}>
-                <Image minH={"280"} src={imageUrl} />
-                <Button variant={"unstyled"} position={"absolute"} top={0} right={0} color={"white"}>
-                    <FaRegHeart size={"20px"} />
-                </Button>
-            </Box>
-            <Box>
-                <Grid gap={2} templateColumns={"10fr 1fr"}>
-                    <Text display={"block"} as={"b"} noOfLines={1} fontSize={"md"}>
-                        {name}
+        <Link to={`/rooms/${pk}`}>
+            <VStack alignItems={"flex-start"}>
+                <Box position={"relative"} overflow={"hidden"} mb={2} rounded={"2xl"}>
+                    <Image minH={"280"} src={imageUrl} />
+                    <Button variant={"unstyled"} position={"absolute"} top={0} right={0} color={"white"}>
+                        <FaRegHeart size={"20px"} />
+                    </Button>
+                </Box>
+                <Box>
+                    <Grid gap={2} templateColumns={"10fr 1fr"}>
+                        <Text display={"block"} as={"b"} noOfLines={1} fontSize={"md"}>
+                            {name}
+                        </Text>
+                        <HStack _hover={{ color: "red.100" }} spacing={1}>
+                            <FaStar size={12} />
+                            <Text fontSize={"sm"}>{rating}</Text>
+                        </HStack>
+                    </Grid>
+                    <Text fontSize={"sm"} color={gray}>
+                        {city}, {country}
                     </Text>
-                    <HStack _hover={{ color: "red.100" }} spacing={1}>
-                        <FaStar size={12} />
-                        <Text fontSize={"sm"}>{rating}</Text>
-                    </HStack>
-                </Grid>
+                </Box>
                 <Text fontSize={"sm"} color={gray}>
-                    {city}, {country}
+                    <Text as={"b"}>${price}</Text> / night
                 </Text>
-            </Box>
-            <Text fontSize={"sm"} color={gray}>
-                <Text as={"b"}>${price}</Text> / night
-            </Text>
-        </VStack>
+            </VStack>
+        </Link>
     );
 }
