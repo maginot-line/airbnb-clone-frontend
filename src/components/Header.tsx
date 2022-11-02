b4454761bbf5910c559a07a6d8311cde00850d20
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-02 10:25:34 +0900

    20.0 useUser

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-import { Box, Button, HStack, IconButton, LightMode, Stack, useColorMode, useColorModeValue, useDisclosure } from "@chakra-ui/react";
+import { Avatar, Box, Button, HStack, IconButton, LightMode, Stack, useColorMode, useColorModeValue, useDisclosure } from "@chakra-ui/react";
 import { FaAirbnb, FaMoon, FaSun } from "react-icons/fa";
+import useUser from "../lib/useUser";
 import LoginModal from "./LoginModal";
 import SignUpModal from "./SignUpModal";
 
 export default function Header() {
+    const { userLoading, isLoggedIn, user } = useUser();
     const { isOpen: isLoginOpen, onClose: onLoginClose, onOpen: onLoginOpen } = useDisclosure();
     const { isOpen: isSignUpOpen, onClose: onSignUpClose, onOpen: onSignUpOpen } = useDisclosure();
     const { toggleColorMode } = useColorMode();
@@ -24,12 +26,20 @@ export default function Header() {
             </Box>
             <HStack spacing={2}>
                 <IconButton onClick={toggleColorMode} variant={"ghost"} aria-label="Toggle dark mode" icon={<Icon />} />
-                <Button onClick={onLoginOpen}>Log in</Button>
-                <LightMode>
-                    <Button onClick={onSignUpOpen} colorScheme={"red"}>
-                        Sign up
-                    </Button>
-                </LightMode>
+                {!userLoading ? (
+                    !isLoggedIn ? (
+                        <>
+                            <Button onClick={onLoginOpen}>Log in</Button>
+                            <LightMode>
+                                <Button onClick={onSignUpOpen} colorScheme={"red"}>
+                                    Sign up
+                                </Button>
+                            </LightMode>
+                        </>
+                    ) : (
+                        <Avatar size={"md"} />
+                    )
+                ) : null}
             </HStack>
             <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
             <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
