2d5705d34515eabb26218d27c1f755131e599fa9
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-02 10:40:01 +0900

    20.1 Credentials

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@
 import { QueryFunctionContext } from "@tanstack/react-query";
 import axios from "axios";
 
-const instance = axios.create({ baseURL: "http://127.0.0.1:8000/api/v1/" });
+const instance = axios.create({ baseURL: "http://127.0.0.1:8000/api/v1/", withCredentials: true });
 
 export const getRooms = () => instance.get("rooms/").then((response) => response.data);
 
