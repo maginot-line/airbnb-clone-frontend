b7b62aa6487acea2801945f7ce80ad34b5f6d715
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-07 16:47:53 +0900

    20.5 Github Log In

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -12,7 +12,12 @@ export default function SocialLogin() {
                 <Divider />
             </HStack>
             <VStack>
-                <Button w={"100%"} leftIcon={<FaGithub />}>
+                <Button
+                    as={"a"}
+                    href={"https://github.com/login/oauth/authorize?client_id=edd37c5f10e12db8f49e&scope=read:user,user:email"}
+                    w={"100%"}
+                    leftIcon={<FaGithub />}
+                >
                     Continue with Github
                 </Button>
                 <Button w={"100%"} leftIcon={<FaComment />} colorScheme={"yellow"}>
