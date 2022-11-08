efdf932395a02709892efb5dee1e362d4ee7596d
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-11-08 17:37:28 +0900

    20.8 Emails

--- src/routes/GithubConfirm.tsx
+++ src/routes/GithubConfirm.tsx
@@ -1,15 +1,24 @@
-import { Heading, Spinner, Text, VStack } from "@chakra-ui/react";
+import { Heading, Spinner, Text, useToast, VStack } from "@chakra-ui/react";
+import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { githubLogin } from "../api";
 
 export default function GithubConfirm() {
     const { search } = useLocation();
+    const toast = useToast();
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
     const confirmLogin = async () => {
         const params = new URLSearchParams(search);
         const code = params.get("code");
         if (code) {
-            await githubLogin(code);
+            const status = await githubLogin(code);
+            if (status === 200) {
+                toast({ status: "success", title: "Welcome!", description: "Happy to have you back!" });
+            }
+            queryClient.refetchQueries(["me"]);
+            navigate("/");
         }
     };
     useEffect(() => {
