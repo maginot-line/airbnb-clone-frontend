28afe3ba55add28983950e1e9e194f0052c749d4
CHOI HYUNWOO <kr.choi.hyunwoo@gmail.com>
2022-12-23 15:07:49 +0900

    21.6 One Time Upload

--- src/api.ts
+++ src/api.ts
@@ -1,73 +1,76 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { QueryFunctionContext } from "@tanstack/react-query";
-import axios from "axios";
-import Cookie from "js-cookie";
-import { ISignIn } from "./types";
+import { QueryFunctionContext } from '@tanstack/react-query';
+import axios from 'axios';
+import Cookie from 'js-cookie';
+import { ISignIn } from './types';
 
 export interface IUsernameLoginVariables {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
 }
 
 export interface IUsernameLoginSuccess {
-    ok: string;
+  ok: string;
 }
 
 export interface IUsernameLoginError {
-    error: string;
+  error: string;
 }
 
-const instance = axios.create({ baseURL: "http://127.0.0.1:8000/api/v1/", withCredentials: true });
+const instance = axios.create({ baseURL: 'http://127.0.0.1:8000/api/v1/', withCredentials: true });
 
-export const getRooms = () => instance.get("rooms/").then((response) => response.data);
+export const getRooms = () => instance.get('rooms/').then((response) => response.data);
 
 export const getRoom = ({ queryKey }: QueryFunctionContext) => {
-    const [_, roomPk] = queryKey;
-    return instance.get(`rooms/${roomPk}/`).then((response) => response.data);
+  const [_, roomPk] = queryKey;
+  return instance.get(`rooms/${roomPk}/`).then((response) => response.data);
 };
 
 export const getRoomReviews = ({ queryKey }: QueryFunctionContext) => {
-    const [_, roomPk] = queryKey;
-    return instance.get(`rooms/${roomPk}/reviews`).then((response) => response.data);
+  const [_, roomPk] = queryKey;
+  return instance.get(`rooms/${roomPk}/reviews`).then((response) => response.data);
 };
 
-export const getMe = () => instance.get("users/me").then((response) => response.data);
+export const getMe = () => instance.get('users/me').then((response) => response.data);
 
 export const logOut = () =>
-    instance.post("users/log-out", null, { headers: { "X-CSRFToken": Cookie.get("csrftoken") || "" } }).then((response) => response.data);
+  instance.post('users/log-out', null, { headers: { 'X-CSRFToken': Cookie.get('csrftoken') || '' } }).then((response) => response.data);
 
 export const githubLogin = (code: string) =>
-    instance.post("users/github", { code }, { headers: { "X-CSRFToken": Cookie.get("csrftoken") || "" } }).then((response) => response.status);
+  instance.post('users/github', { code }, { headers: { 'X-CSRFToken': Cookie.get('csrftoken') || '' } }).then((response) => response.status);
 
 export const kakaoLogin = (code: string) =>
-    instance.post("users/kakao", { code }, { headers: { "X-CSRFToken": Cookie.get("csrftoken") || "" } }).then((response) => response.status);
+  instance.post('users/kakao', { code }, { headers: { 'X-CSRFToken': Cookie.get('csrftoken') || '' } }).then((response) => response.status);
 
 export const usernameLogIn = ({ username, password }: IUsernameLoginVariables) =>
-    instance.post("users/log-in", { username, password }, { headers: { "X-CSRFToken": Cookie.get("csrftoken") || "" } }).then((response) => response.data);
+  instance.post('users/log-in', { username, password }, { headers: { 'X-CSRFToken': Cookie.get('csrftoken') || '' } }).then((response) => response.data);
 
 export const signIn = ({ name, email, username, password }: ISignIn) =>
-    instance
-        .post("users/sign-in", { name, email, username, password }, { headers: { "X-CSRFToken": Cookie.get("csrftoken") || "" } })
-        .then((response) => response.data);
+  instance
+    .post('users/sign-in', { name, email, username, password }, { headers: { 'X-CSRFToken': Cookie.get('csrftoken') || '' } })
+    .then((response) => response.data);
 
-export const getAmenities = () => instance.get("rooms/amenities").then((response) => response.data);
+export const getAmenities = () => instance.get('rooms/amenities').then((response) => response.data);
 
-export const getCategories = () => instance.get("categories").then((response) => response.data);
+export const getCategories = () => instance.get('categories').then((response) => response.data);
 
 export interface IUploadRoomVariables {
-    name: string;
-    country: string;
-    city: string;
-    price: number;
-    rooms: number;
-    toilets: number;
-    description: string;
-    address: string;
-    pet_friendly: boolean;
-    kind: string;
-    amenities: string[];
-    category: string;
+  name: string;
+  country: string;
+  city: string;
+  price: number;
+  rooms: number;
+  toilets: number;
+  description: string;
+  address: string;
+  pet_friendly: boolean;
+  kind: string;
+  amenities: string[];
+  category: string;
 }
 
 export const uploadRoom = (variables: IUploadRoomVariables) =>
-    instance.post("rooms/", variables, { headers: { "X-CSRFToken": Cookie.get("csrftoken") || "" } }).then((response) => response.data);
+  instance.post('rooms/', variables, { headers: { 'X-CSRFToken': Cookie.get('csrftoken') || '' } }).then((response) => response.data);
+
+export const getUploadURL = () =>
+  instance.post('medias/photos/get-url', null, { headers: { 'X-CSRFToken': Cookie.get('csrftoken') || '' } }).then((response) => response.data);
--- src/routes/UploadPhotos.tsx
+++ src/routes/UploadPhotos.tsx
@@ -1,28 +1,42 @@
-import { Box, Button, Container, FormControl, Heading, Input, VStack } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
-import { useParams } from "react-router-dom";
-import ProtectedPage from "../components/ProtectedPage";
-import useHostOnlyPage from "../components/HostOnlyPage";
+import { Box, Button, Container, FormControl, Heading, Input, VStack } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import ProtectedPage from '../components/ProtectedPage';
+import useHostOnlyPage from '../components/HostOnlyPage';
+import { getUploadURL } from '../api';
+import { useMutation } from '@tanstack/react-query';
+
+interface IForm {
+  file: FileList;
+}
 
 export default function UploadPhotos() {
-    const { register, watch } = useForm();
-    const { roomPk } = useParams();
-    useHostOnlyPage();
-    return (
-        <ProtectedPage>
-            <Box pb={40} mt={10} px={{ base: 10, lg: 40 }}>
-                <Container>
-                    <Heading textAlign={"center"}>Upload a Photo</Heading>
-                    <VStack spacing={5} mt={10}>
-                        <FormControl>
-                            <Input {...register("file")} type="file" accept="image/*" />
-                        </FormControl>
-                        <Button w="full" colorScheme={"red"}>
-                            Upload photos
-                        </Button>
-                    </VStack>
-                </Container>
-            </Box>
-        </ProtectedPage>
-    );
+  const { register, handleSubmit } = useForm<IForm>();
+  const mutation = useMutation(getUploadURL, {
+    onSuccess: (data: any) => {
+      console.log(data);
+    },
+  });
+  const { roomPk } = useParams();
+  useHostOnlyPage();
+  const onSubmit = (data: any) => {
+    mutation.mutate();
+  };
+  return (
+    <ProtectedPage>
+      <Box pb={40} mt={10} px={{ base: 10, lg: 40 }}>
+        <Container>
+          <Heading textAlign={'center'}>Upload a Photo</Heading>
+          <VStack as='form' onSubmit={handleSubmit(onSubmit)} spacing={5} mt={10}>
+            <FormControl>
+              <Input {...register('file')} type='file' accept='image/*' />
+            </FormControl>
+            <Button type='submit' w='full' colorScheme={'red'}>
+              Upload photos
+            </Button>
+          </VStack>
+        </Container>
+      </Box>
+    </ProtectedPage>
+  );
 }
